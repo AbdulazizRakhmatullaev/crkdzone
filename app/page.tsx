68cbe101c900a7dd03961acbf93d0eeee79bf771
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-10-26 03:43:36 +0500

    bug fixes

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="title">
+        <div className="grid"></div>
+        <div className="tRow">Base</div>
+      </div>
+      <div className="homcon">
+        <div className="balance">
+          <div className="coinscnt">Soon</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
