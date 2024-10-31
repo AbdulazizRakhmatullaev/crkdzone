fcdcaf2c48edb69733f84b2da76550df36172ddb
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-10-31 06:10:32 +0500

    bug fixes

---
+++ globals.d.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+    var prisma: PrismaClient | undefined; // Allow it to be undefined initially
+}
+
+interface HapticFeedback {
+    impactOccurred(type: "light" | "medium" | "heavy"): void;
+}
+
+// Define types for Telegram and WebApp
+interface TelegramWebApp {
+    initData: string;
+    ready(callback: () => void): void;
+    expand(): void;
+    disableVerticalSwipes(): void;
+    platform: string;
+    HapticFeedback: HapticFeedback;
+}
+
+interface Telegram {
+    WebApp: TelegramWebApp;
+}
+
+// Extend the global window object
+declare global {
+    interface Window {
+        Telegram: Telegram;
+    }
+}
--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,16 @@
-// 
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "development") {
+  // In development, create a new Prisma Client for every request
+  prisma = new PrismaClient();
+} else {
+  // In production, reuse the Prisma Client to avoid issues with serverless environments
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
