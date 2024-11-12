0326e403f85a9b879bbce80e0e9b19b8b9c2c939
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-11-12 15:14:29 +0500

    bug fixes

--- app/(pages)/equipment/page.tsx
+++ app/(pages)/equipment/page.tsx
@@ -3,7 +3,7 @@ import Title from "@/app/components/title"
 export default function Equipment() {
   return (
     <>
-      <Title name="Rankings" />
+      <Title name="Equipment" />
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Title from "@/app/components/title"
 const Home = () => {
   return (
     <>
-      <Title name="Rankings" />
+      <Title name="Base" />
     </>
   );
 };
