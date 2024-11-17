b91a5f2768dd5e54fcc5ff69899f68f24d1fe0f5
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-11-17 22:46:51 +0500

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       webApp.disableVerticalSwipes();
 
       if (platform === "ios" || platform === "android") {
-        webApp.requestFullScreen()
+        webApp.requestFullscreen();
       }
 
       setPlatform(platform);
--- globals.d.ts
+++ globals.d.ts
@@ -30,7 +30,7 @@ interface TelegramWebApp {
     };
     ready(callback: () => void): void;
     expand(): void;
-    requestFullScreen(): void;
+    requestFullscreen(): void;
     disableVerticalSwipes(): void;
     platform: string;
     HapticFeedback: HapticFeedback;
