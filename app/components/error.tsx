ecd38ef12d5ed7962c70a31dcd04deb26deefc7e
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-11-20 11:45:38 +0500

    bug fixes

---
+++ app/components/error.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image';
+
+export default function Error() {
+    return (
+        <div className='fl flex-col justify-center items-center px-5 h-full bg-black'>
+            <Image
+                src="/noUsername.jpg"
+                alt="img"
+                priority={true}
+                width={250}
+                className='mb-10'
+            />
+            <div className='text-xl uppercase'>Soldier</div>
+            <div className="text-center">
+                We are sorry for the inconvenience,
+                <br /> We are working on this issue, you can come back later.
+            </div>
+        </div>
+    )
+}
--- app/contexts/user.tsx
+++ app/contexts/user.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
-import Image from 'next/image';
 
 interface User {
     id: number
@@ -19,7 +18,6 @@ export const UserContext = createContext<UserContextType | undefined>(undefined)
 
 export function UserProvider({ children }: { children: ReactNode }) {
     const [user, setUser] = useState<User | null>(null);
-    const [noUsername, setNoUsername] = useState(true);
 
     useEffect(() => {
         if (window.Telegram?.WebApp) {
@@ -29,56 +27,40 @@ export function UserProvider({ children }: { children: ReactNode }) {
 
             const params = new URLSearchParams(initData);
             const tg_id = params.get("user") ? JSON.parse(params.get("user")!).id : null;
-            const username = params.get("user") ? JSON.parse(params.get("user")!).username : undefined;
+            const firstName = dataUnsafe.user?.first_name;
+            const lastName = dataUnsafe.user?.last_name;
+            const avatar_url = dataUnsafe?.user?.photo_url;
+            let username = params.get("user") ? JSON.parse(params.get("user")!).username : undefined;
 
-            if (username !== undefined && process.env.NODE_ENV === "production") {
-                setNoUsername(false);
+            if (username === undefined) {
+                username = firstName + " " + lastName
+            }
 
-                const fetchUser = async () => {
-                    try {
-                        const avatar_url = dataUnsafe?.user?.photo_url;
+            const fetchUser = async () => {
+                try {
+                    const res = await fetch("/api/check-user", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ tg_id, username, avatar_url })
+                    });
+                    if (!res.ok) throw new Error("Unable to run check-user.");
 
-                        const res = await fetch("/api/check-user", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify({ tg_id, username, avatar_url })
-                        });
-                        if (!res.ok) throw new Error("Unable to run check-user.");
-
-                        const user = await res.json();
-                        setUser(user);
-                    } catch (error) {
-                        console.error(error);
-                    }
+                    const user = await res.json();
+                    setUser(user);
+                } catch (error) {
+                    console.error(error);
                 }
-                
-                fetchUser();
-            };
+            }
+            
+            fetchUser();
         }
     }, [])
 
     return (
         <UserContext.Provider value={{ user }}>
-            {noUsername && process.env.NODE_ENV === "production" ? (
-                <div className='fl flex-col justify-center items-center px-5 h-full bg-black'>
-                    <Image
-                        src="/noUsername.jpg"
-                        alt="img"
-                        priority={true}
-                        width={250}
-                        className='mb-10'
-                    />
-                    <div className='text-xl uppercase'>Soldier?</div>
-                    <div className="text-center">
-                        We are unable to recognize you,
-                        <br /> Come back with your username on you!
-                    </div>
-                </div>
-            ) : (
-                children
-            )}
+            {children}
         </UserContext.Provider>
     );
 }
