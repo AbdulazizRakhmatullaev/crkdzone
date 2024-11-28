e662d56cffdeebfb5d41f020468103cd49984fc8
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-11-28 22:03:05 +0500

    bug fixes

--- app/contexts/initData.tsx
+++ app/contexts/initData.tsx
@@ -105,7 +105,7 @@ export function InitDataProvider({ children }: { children: ReactNode }) {
     
         //     fetchUser();
         // }
-    }, [])
+    }, [platform])
 
     return (
         <InitDataContext.Provider value={{ user, platform }}>
