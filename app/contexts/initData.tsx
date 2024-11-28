a3fbd83bf03f22e7834d83157cced23288e810c0
Rakhmatullaev Abdulaziz <frrhyk@gmail.com>
2024-11-28 22:28:19 +0500

    bug fixes

--- app/contexts/initData.tsx
+++ app/contexts/initData.tsx
@@ -14,14 +14,14 @@ interface User {
 }
 
 interface InitContextType {
-    user: User | undefined;
+    user: User | null;
     platform: string | undefined;
 }
 
 export const InitDataContext = createContext<InitContextType | undefined>(undefined);
 
 export function InitDataProvider({ children }: { children: ReactNode }) {
-    const [user, setUser] = useState<User | undefined>(undefined);
+    const [user, setUser] = useState<User | null>(null);
     const [loading, setLoading] = useState(true);
     const [platform, setPlatform] = useState<string | undefined>(undefined);
 
@@ -33,12 +33,6 @@ export function InitDataProvider({ children }: { children: ReactNode }) {
         script.onload = () => {
             if (window?.Telegram?.WebApp) {
                 const webApp = window.Telegram?.WebApp;
-                const initData = webApp.initData;
-                const dataUnsafe = webApp.initDataUnsafe;
-                const params = new URLSearchParams(initData);
-                const tg_id = params.get("user") ? JSON.parse(params.get("user")!).id : null;
-                const firstName = dataUnsafe.user?.first_name;
-                const pic = dataUnsafe?.user?.photo_url;
 
                 webApp.ready();
                 webApp.expand();
@@ -49,89 +43,64 @@ export function InitDataProvider({ children }: { children: ReactNode }) {
                 } else {
                     setPlatform("dsk");
                 }
-
-                const fetchUser = async () => {
-                    try {
-                        const res = await fetch("/api/check-user", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify({ tg_id, firstName, pic })
-                        });
-                        if (!res.ok) throw new Error("Unable to run check-user.");
-
-                        const user = await res.json();
-                        setUser(user);
-                    } catch (error) {
-                        console.error(error);
-                    }
-                }
-
-                fetchUser();
             };
         };
 
         document.head.appendChild(script);
-
-        if (process.env.NODE_ENV === "production") {
-            setTimeout(() => {
-                setLoading(false);
-            }, 1600);
-        } else {
-            setLoading(false);
-        }
     }, [platform]);
 
-    // useEffect(() => {
-    //     if (window.Telegram?.WebApp) { // && process.env.NODE_ENV === "production") {
-    //         const webApp = window.Telegram.WebApp;
-    //         const initData = webApp.initData;
-    //         const dataUnsafe = webApp.initDataUnsafe;
+    useEffect(() => {
+        if (window.Telegram?.WebApp && process.env.NODE_ENV === "production") {
+            const webApp = window.Telegram.WebApp;
+            const initData = webApp.initData;
+            const dataUnsafe = webApp.initDataUnsafe;
 
-    //         const params = new URLSearchParams(initData);
-    //         const tg_id = params.get("user") ? JSON.parse(params.get("user")!).id : null;
-    //         const firstName = dataUnsafe.user?.first_name;
-    //         const pic = dataUnsafe?.user?.photo_url;
+            const params = new URLSearchParams(initData);
+            const tg_id = params.get("user") ? JSON.parse(params.get("user")!).id : null;
+            const firstName = dataUnsafe.user?.first_name;
+            const pic = dataUnsafe?.user?.photo_url;
 
-    //         const fetchUser = async () => {
-    //             try {
-    //                 const res = await fetch("/api/check-user", {
-    //                     method: "POST",
-    //                     headers: {
-    //                         "Content-Type": "application/json",
-    //                     },
-    //                     body: JSON.stringify({ tg_id, firstName, pic })
-    //                 });
-    //                 if (!res.ok) throw new Error("Unable to run check-user.");
+            const fetchUser = async () => {
+                try {
+                    const res = await fetch("/api/check-user", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({ tg_id, firstName, pic })
+                    });
+                    if (!res.ok) throw new Error("Unable to run check-user.");
 
-    //                 const user = await res.json();
-    //                 setUser(user);
-    //             } catch (error) {
-    //                 console.error(error);
-    //             }
-    //         }
+                    const user = await res.json();
+                    setUser(user);
+                } catch (error) {
+                    console.error(error);
+                }
+            }
 
-    //         fetchUser();
-    //     }
-    //     // } 
-    //     // else {
-    //     //     const fetchUser = async () => {
-    //     //         try {
-    //     //             const res = await fetch(`/api/user?tg_id=${336417426}`);
-    //     //             if (!res.ok) throw new Error("Unable to run check-user.");
-    
-    //     //             const [user] = await res.json();
-    //     //             console.log(user)
-    //     //             setUser(user);
-    //     //         } catch (error) {
-    //     //             console.error(error);
-    //     //         }
-    //     //     }
-    
-    //     //     fetchUser();
-    //     // }
-    // }, [platform])
+            fetchUser();
+        }
+        // else {
+        //     const fetchUser = async () => {
+        //         try {
+        //             const res = await fetch(`/api/user?tg_id=${336417426}`);
+        //             if (!res.ok) throw new Error("Unable to run check-user.");
+
+        //             const [user] = await res.json();
+        //             console.log(user)
+        //             setUser(user);
+        //         } catch (error) {
+        //             console.error(error);
+        //         }
+        //     }
+
+        //     fetchUser();
+        // }
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 1600);
+    }, [])
 
     return (
         <InitDataContext.Provider value={{ user, platform }}>
